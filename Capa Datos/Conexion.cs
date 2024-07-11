17ee92bdab286c4bf81fc6c90408e39e8b8e7cde
CarlosCamachoU <carloscesarca9@gmail.com>
2024-07-10 21:14:58 -0500

    CAMBIO SOLO DE NOMBRE DE BD

--- Capa Datos/Conexion.cs
+++ Capa Datos/Conexion.cs
@@ -19,7 +19,7 @@ namespace Capa_Datos
         public SqlConnection Conectar()
         {
             SqlConnection cn = new SqlConnection();
-            cn.ConnectionString = "Data Source=DESKTOP-TFH3G9D; Initial Catalog = GIMNASIOJSBD;" +//"User ID=sa;Password = 123";
+            cn.ConnectionString = "Data Source=DESKTOP-MKORCPQ; Initial Catalog = GIMNASIOJSBD;" +//"User ID=sa;Password = 123";
                                   "Integrated Security=true";
             return cn;
         }
